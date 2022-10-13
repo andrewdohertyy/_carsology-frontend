669903a981a4843f56c2f43d6720bc02399a3490
Andrew Doherty <andrewdoherty1996@hotmail.co.uk>
2022-10-13 21:36:18 +0100

    getting page ready for routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import Footer from "./components/Footer/Footer";
 import Main from "./containers/Main/Main";
 import Nav from "./containers/Nav/Nav";
+// import { BrowserRouter as Router, Routes, Route } from "react";
 
 function App() {
-
   console.log("i work");
 
   return (
-    <>
-      <Nav title={"_courseology"}/>
-      <Main />
-      <Footer />
-     </>
-    );
+      <>
+          <Nav title={"_courseology"} />
+          
+          <Main />
+       
+          <Footer />
+      </>
+   
+  );
 }
 
 export default App;
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,27 +3,16 @@ import "./Footer.scss";
 
 const Footer = () => {
   return (
-    <div>
-      <footer class="footer">
-
-        <div class="footer__left">
-          <p class="footer__links">
-            <a class="link-1" href="#">
-              Home
-            </a>
-
-            <a href="#">Pricing</a>
-
-            <a href="#">About</a>
-
-            <a href="#">Faq</a>
-
-            <a href="#">Contact</a>
-          </p>
-
-          <p>_courseology &copy; 2022</p>
-        </div>
-      </footer>
+    <div className="footer">
+      <h3 className="footer__text">
+        SIGN UP TO OUR MAILING LIST HERE
+        </h3>
+      <input
+        className="footer__input"
+        type="text"
+        placeholder="Enter your email address..."
+      />
+      <button className="footer__button">Sign Up</button>
     </div>
   );
 };
