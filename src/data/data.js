261d9a112e4d94819d37c4e8747344b5718d266d
Andrew Doherty <andrewdoherty1996@hotmail.co.uk>
2022-10-22 20:01:37 +0100

    refactored code

--- src/data/data.js
+++
@@ -1,9 +0,0 @@
-let courseInfo = {
-    Name: "nology",
-    Category: "Tech",
-    Info: "lorem ipsum dolor sit amet, consectetur adipiscing",
-    CompletionTime: "3 months",
-    Price: "£9000",
-    Syllabus: "Full-stack development",
-    Author: "Rob Hooper"
-};
