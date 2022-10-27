eb138192edddae9da9b799c115f5f5440d14fab3
Andrew Doherty <andrewdoherty1996@hotmail.co.uk>
2022-10-27 19:51:37 +0100

    installed firebase to start

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCzUL0Rx5mgHn8lSerR3KB2q0Ww0Bmt_ys",
+  authDomain: "carsology.firebaseapp.com",
+  projectId: "carsology",
+  storageBucket: "carsology.appspot.com",
+  messagingSenderId: "778849303086",
+  appId: "1:778849303086:web:7c478ebb3b239f68187dbf",
+  measurementId: "G-BGZRN06PJW"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
